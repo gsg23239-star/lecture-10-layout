327a2c6460611b81dee92ddf765f217fd72a0ce0
LINE <gsg2323@naver.com>
2026-04-27 17:44:02 +0900

    초기화

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {RouterProvider} from "react-router";
+import GlobalStyle from "./GlobalStyle.ts";
+import { AppRouter } from "./router/AppRouter.tsx";
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <RouterProvider router={AppRouter()} />
+        </>
+    );
+}
+
+export default App;
